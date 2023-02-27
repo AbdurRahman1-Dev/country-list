a685180b39f65fb1de3a7b58eefb4ee908387898
--add <rahmanaky521@gmail.com>
2023-02-27 15:25:41 +0600

    show all

--- countries.js
+++ countries.js
@@ -1,15 +1,15 @@
 const loadCountries = () =>{
   fetch('https://restcountries.com/v2/all')
       .then(res => res.json())
-      .then(json => display(json))
+      .then(json => display(json.slice(0, 4)))
 }
 
 const display = (data) => {
-
   var divContainer = document.getElementById('country-container')
     data.map(x => {
       let {name, capital, region, population} = x
       let flag = x.flags.png;
+      console.log(x.cioc);
       const countryDiv = document.createElement('div');
       countryDiv.classList.add("countryStyle")
       countryDiv.innerHTML = `
@@ -20,8 +20,13 @@ const display = (data) => {
         <h3>Region: ${region}</h3>
         <h4>Population: ${population}</h4>
       `;
-    
       divContainer.appendChild(countryDiv)
     })
+}
+
+const showAll = () => {
+  fetch('https://restcountries.com/v2/all')
+      .then(res => res.json())
+      .then(json => display(json.slice(4)))
 }
 loadCountries()
